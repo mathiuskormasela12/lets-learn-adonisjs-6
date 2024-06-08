72488b3810d7c8a4da32f4ccb7b909c4ef29db05
Matthew Kormasela <mathiuskormasela12@gmail.com>
2024-06-09 00:22:05 +0700

    learning router

--- start/routes.ts
+++ start/routes.ts
@@ -10,3 +10,17 @@
 import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
+
+router.get('/movie', (ctx) => {
+  // To return string
+  // return 'Hello Movie'
+
+  // First way to add params to the page
+  ctx.view.share({ title: 'Queen of tears' })
+
+  // To render view from Edge template engine
+  // return ctx.view.render('pages/movie')
+
+  // To render a view and send a params in the same time
+  return ctx.view.render('pages/movie', { title: 'Boruto' })
+})
