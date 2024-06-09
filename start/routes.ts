23c54ef2553f479a94bec779f39dcd85ae9eb435
Matthew Kormasela <mathiuskormasela12@gmail.com>
2024-06-09 19:12:27 +0700

    navigate between route

--- start/routes.ts
+++ start/routes.ts
@@ -9,18 +9,29 @@
 
 import router from '@adonisjs/core/services/router'
 
-router.on('/').render('pages/home')
+router.on('/').render('pages/home').as('home.index')
 
-router.get('/movie', (ctx) => {
-  // To return string
-  // return 'Hello Movie'
+router
+  .get('/movie', (ctx) => {
+    // To return string
+    // return 'Hello Movie'
 
-  // First way to add params to the page
-  ctx.view.share({ title: 'Queen of tears' })
+    // First way to add params to the page
+    ctx.view.share({ title: 'Queen of tears' })
 
-  // To render view from Edge template engine
-  // return ctx.view.render('pages/movie')
+    // To render view from Edge template engine
+    // return ctx.view.render('pages/movie')
 
-  // To render a view and send a params in the same time
-  return ctx.view.render('pages/movie', { title: 'Boruto' })
-})
+    // To render a view and send a params in the same time
+    return ctx.view.render('pages/movie', { title: 'Boruto' })
+  })
+  .as('movie.index')
+
+router
+  .get('/user', (ctx) => {
+    ctx.view.share({
+      name: 'Sarada',
+    })
+    return ctx.view.render('pages/user')
+  })
+  .as('user.index') // To give a name/aliases for a route to use route method on edge template engine
