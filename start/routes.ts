68cca6f42d831695d740c5ad71c23db5d1120846
Matthew Kormasela <mathiuskormasela12@gmail.com>
2024-06-09 20:46:32 +0700

    dynamic router, vite, custom error message, and validate route params

--- start/routes.ts
+++ start/routes.ts
@@ -7,6 +7,32 @@
 |
 */
 
+import { Exception } from '@adonisjs/core/exceptions'
 import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
+
+router
+  .get('/movies', (ctx) => {
+    return ctx.view.render('pages/movies')
+  })
+  .as('movies')
+
+router
+  .get('movie/:id', (ctx) => {
+    // To read req.params (will return any type)
+    const id = Number(ctx.params.id)
+
+    if (id < 1) {
+      // To throw custome error message
+      throw new Exception('Your movie id should be greated than 0', {
+        code: 'INVALID_MOVIE_ID',
+        status: 400,
+      })
+    }
+
+    const header = '<p>This is paragraph</p>'
+    return ctx.view.render('pages/movie', { id, header })
+  })
+  .as('movie')
+  .where('id', router.matchers.number()) // to validate route params
